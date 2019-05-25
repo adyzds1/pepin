0506d7b2ea4e30881543ceed1f780cc5884a2b62
student <you@example.com>
2019-05-25 14:57:12 +0300

    relevant message

---
+++ exercitiu4.js
@@ -0,0 +1,7 @@
+ const fs = require('fs'); 
+    fs.readFile('student.json', (err, data) => { 
+        if (err) throw err; 
+        let student = JSON.parse(data); 
+        console.log(student); }); 
+    console.log('This is after the read call'); 
+
